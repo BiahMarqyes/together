b39cf4b505a3c30a443dc77dae2413da09e76836
BiahMarqyes <santosmarquesbeatriz@gmail.com>
2023-06-01 12:25:47 -0300

    a

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,18 @@ export class Tab1Page {
 
   constructor() {}
 
+  data = new Date();
+
+  dia = String(this.data.getDate()).padStart(2,  '0');
+
+  mes = String(this.data.getMonth() + 1).padStart(2,  '0');
+
+  dataAtual = `${this.dia}/${this.mes}`;
+
+  inputFile = document.querySelector('#picture__input');
+
+  pictureImage = document.querySelector('.picture__image');
+
+
+
 }
